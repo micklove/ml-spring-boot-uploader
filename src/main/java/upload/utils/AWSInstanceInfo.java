e47a22bb50a26961b970766fc0c8c56f07cbfdea
micklove <micklove+github@gmail.com>
2016-05-19 23:16:48 +1000

    add tests

--- src/main/java/upload/utils/AWSInstanceInfo.java
+++ src/main/java/upload/utils/AWSInstanceInfo.java
@@ -1,5 +1,7 @@
 package upload.utils;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -13,44 +15,43 @@ import org.springframework.web.client.RestTemplate;
  */
 public class AWSInstanceInfo {
 
-
-	Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	/**
-	 * @Autowired
-	public DummyService(@Value("${some.constructor.property}") String path) {}
-	 * @param restTemplate
-	 */
-	@Autowired
-	public AWSInstanceInfo(RestTemplate restTemplate,
-	                       @Value("${aws.instance.document.address}") String path) {
-		this.restTemplate = restTemplate;
-		this.path = path;
-	}
-
-
 	/**
 	 * http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
 	 */
 	private final RestTemplate restTemplate;
-	private final String path;
+	private final URI path;
+	private static Logger logger = LoggerFactory.getLogger(AWSInstanceInfo.class);
+	private Map<String, String> response = new HashMap<>();
 
-
-	//http://stackoverflow.com/questions/24208828/resttemplate-getforobject-map-responsetype
-	public Map<String, String> getInstanceInfo() {
-		Map<String, String> response = restTemplate.getForObject(path, Map.class);
-//		String json = restTemplate.getForObject(path, String.class);
-//		Map<String,String> map = new HashMap<String,String>();
-//		ObjectMapper mapper = new ObjectMapper();
-//
-//		try {
-//			//convert JSON string to Map
-//			map = mapper.readValue(json, new TypeReference<HashMap<String,String>>(){});
-//		} catch (Exception e) {
-//
-//			logger.info("Exception converting {} to map", json, e);
-//		}
-
-		return response;
+	@Autowired
+	public AWSInstanceInfo(final RestTemplate restTemplate,
+	                       @Value("${aws.instance.document.address}")
+	                       final URI path) {
+		this.restTemplate = restTemplate;
+		this.path = path;
+		initAWSInstanceInfo();
 	}
+
+	/**
+	 * Make a GET request to grab the AWS Instance Info.
+	 * nb: Failed attempts to get the info will be ignored.
+	 *
+	 * @return the AWS instance response, as a map
+	 */
+	private void initAWSInstanceInfo() {
+		try {
+			response = restTemplate.getForObject(path, Map.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("\n\nUnable to get AWS instance info from {}", path);
+		}
+	}
+
+	public String getAWSInstancePropertyByKey(final String key) {
+		return response.get(key);
+	}
+
+	//TODO - Get Instance TAGS
+
+
 }
--- src/test/java/upload/utils/AWSInstanceInfoTest.java
+++ src/test/java/upload/utils/AWSInstanceInfoTest.java
@@ -1,71 +1,120 @@
 package upload.utils;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.tika.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
-/**
- * Created by IntelliJ IDEA.
- * User: micklove
- * Date: 19/05/2016
- * Time: 12:00 AM
- */
+@RunWith(MockitoJUnitRunner.class)
 public class AWSInstanceInfoTest {
 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Mock
-	RestTemplate restTemplate;
+	private static String devpayProductCodes =  null;
+	private static String availabilityZone =  "us-east-1d";
+	private static String privateIp =  "10.158.112.84";
+	private static String version =  "2010-08-31";
+	private static String region =  "us-east-1";
+	private static String instanceId =  "i-1234567890abcdef0";
+	private static String billingProducts =  null;
+	private static String instanceType =  "t1.micro";
+	private static String accountId =  "123456789012";
+	private static String pendingTime =  "2015-11-19T16:32:11Z";
+	private static String imageId =  "ami-5fb8c835";
+	private static String kernelId =  "aki-919dcaf8";
+	private static String ramdiskId =  null;
+	private static String architecture =  "x86_64";
 
-	final String path="";
+	@Mock
+	private RestTemplate restTemplate;
+	private final String instanceInfoEndpoint = "http://blah.blah/aws";
+	private URI uri;
+	private AWSInstanceInfo awsInstanceInfo;
 
 
 	@Before
 	public void setUp() throws Exception {
 
-		ClassLoader classLoader = getClass().getClassLoader();
-		String result = IOUtils.toString(classLoader.getResourceAsStream("json/example-instance-document.json"));
-		System.out.println(result);
+		initMocks(this);
 
-		String mockJsonResponse = new String(Files.readAllBytes(Paths.get("json/example-instance-document.json")));
-		System.out.println(mockJsonResponse);
-		Map<String, String> response = getMapFromJsonString(mockJsonResponse);
-		when(restTemplate.getForObject(any(String.class), Map.class)).thenReturn(response);
+		Map<String, String> mockResponse = getMockInstanceMap();
+		when(restTemplate.getForObject(any(URI.class), anyObject())).thenReturn(mockResponse);
+		uri = new URI(instanceInfoEndpoint);
+		awsInstanceInfo = new AWSInstanceInfo(restTemplate, uri);
+	}
+
+
+	@Test
+	public void given_instance_info_available_when_unknown_key_requested_then_return_null() throws Exception {
+		assertThat(awsInstanceInfo.getAWSInstancePropertyByKey("blah"), is(nullValue()));
+	}
+
+	@Test
+	public void given_instance_info_available_when_private_ip_requested_then_return_value() throws Exception {
+		assertThat(awsInstanceInfo.getAWSInstancePropertyByKey("privateIp"), is(privateIp));
+	}
+
+	@Test
+	public void given_rest_api_call_fails_when_key_requested_then_return_empty_value() throws Exception {
+		initMocks(this);
+		when(restTemplate.getForObject(any(URI.class), anyObject())).thenThrow(RuntimeException.class);
+		awsInstanceInfo = new AWSInstanceInfo(restTemplate, uri);
+		assertThat(awsInstanceInfo.getAWSInstancePropertyByKey("privateIp"), is(nullValue()));
 	}
 
 	public Map<String, String> getMapFromJsonString(final String json) {
 		ObjectMapper mapper = new ObjectMapper();
-		Map<String,String> map = new HashMap<String,String>();
+		Map<String, String> map = new HashMap<>();
 
 		try {
 			//convert JSON string to Map
 			map = mapper.readValue(json, new TypeReference<HashMap<String,String>>(){});
 		} catch (Exception e) {
-
 			logger.info("Exception converting {} to map", json, e);
 		}
 		return map;
 	}
 
 
-	@Test
-	public void given_instance_info_available_when_requested_then_return_instance_info() throws Exception {
-		assertThat(new AWSInstanceInfo(restTemplate, path).getInstanceInfo(), not(nullValue()));
+
+	/**
+	 * Get Instance Properties as Map
+	 * @return
+	 */
+	protected static Map<String, String> getMockInstanceMap() {
+		final Map<String, String> instancePropertiesMap = new HashMap<>();
+		instancePropertiesMap.put( "devpayProductCodes",    devpayProductCodes);
+		instancePropertiesMap.put( "availabilityZone",      availabilityZone);
+		instancePropertiesMap.put( "privateIp",             privateIp);
+		instancePropertiesMap.put( "version",               version);
+		instancePropertiesMap.put( "region",                region);
+		instancePropertiesMap.put( "instanceId",            instanceId);
+		instancePropertiesMap.put( "billingProducts",       billingProducts);
+		instancePropertiesMap.put( "instanceType",          instanceType);
+		instancePropertiesMap.put( "accountId",             accountId);
+		instancePropertiesMap.put( "pendingTime",           pendingTime);
+		instancePropertiesMap.put( "imageId",               imageId);
+		instancePropertiesMap.put( "kernelId",              kernelId);
+		instancePropertiesMap.put( "ramdiskId",             ramdiskId);
+		instancePropertiesMap.put( "architecture",          architecture);
+		//return Collections.unmodifiableMap(instancePropertiesMap);
+		return instancePropertiesMap;
 	}
 }
